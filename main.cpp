debfbc4053f4915ee120fe12d1771402ef55a84e
Pranav Rao <pranavanrao@gmail.com>
2024-05-16 17:36:40 +0530

    Add image url

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <QApplication>
 #include <QAxWidget>
 #include <QMessageBox>
+#include <QDesktopWidget>
 
 int main(int argc, char *argv[])
 {
@@ -11,11 +12,19 @@ int main(int argc, char *argv[])
 
     QAxWidget* ieWidget = new QAxWidget(&w);
 
-    // Set the CLSID for Internet Explorer control
+    QSize mainWindowSize = w.size();
+
     if (!ieWidget->setControl("{8856F961-340A-11D0-A96B-00C04FD705A2}")) {
         QMessageBox::critical(nullptr, "Error", "Could not initiate Internet Explorer ActiveX control");
         return -1;
     }
+
+    ieWidget->setGeometry(0, 0, mainWindowSize.width(), mainWindowSize.height());
+
+    QString url = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Qt_logo_neon_2022.svg/1200px-Qt_logo_neon_2022.svg.png"; // Replace with your URL
+    ieWidget->dynamicCall("Navigate(const QString&)", url);
+
+     w.setCentralWidget(ieWidget);
 
     w.show();
     return a.exec();
